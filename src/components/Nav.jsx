e41db9f2edb3054bcf6024e157c0fb984b3bc0e3
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-04-15 12:52:51 -0700

    Added Resume

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
+import resume from '../assets/resume.pdf'
 
 const Nav = () => {
     return ( 
     <nav className="nav sticky ">
       <ul className="list nav__list ">
-        <li className="nav__item"><HashLink to="/#projects">Projects</HashLink></li>
-        <li className="nav__item"><HashLink to="#skills">Skills</HashLink></li>
-        <li className="nav__item "><a className="contact" href="mailto: jonathan.plaisted@outlook.com">Contact</a></li>
+        <li className="nav__item"><HashLink to="/#projects">PROJECTS</HashLink></li>
+        <li className="nav__item"><HashLink to="#skills">SKILLS</HashLink></li>
+        <li className="nav__item "><a className="contact" href="mailto: jonathan.plaisted@outlook.com">CONTACT</a></li>
+        <li className="nav__item "><a href={resume} target="_blank" rel="noreferrer">RESUME</a></li>
       </ul>
     </nav>
      );
