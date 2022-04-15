75156c2dbfa5c0d8b7dedd360ae588dd6aa6dd0f
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-04-15 13:56:58 -0700

    Icon placement

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,17 @@ import Sprite from '../assets/icon_sprite.svg'
 const Contact = () => {
     return ( 
         <section id="contact">
+            <div className="callout callout--primary">
+                <div className="grid grid--1x2" data-aos="fade-up-left">
+                <div className="callout__content">
+                 <h2 className="callout__heading">Ready to Get in touch?</h2>
+                 <p>
+                   I am eager to work with you and create great technology. 
+                  </p>
+                 </div>
+                  <a href="mailto: jonathan.plaisted@outlook.com" className="btn btn--stretched">Contact</a>
+              </div>
+             </div>
             <div className="icons" data-aos="fade-up">
                 <span className="icon-container">
                     <a href="https://www.linkedin.com/in/jonathan-plaisted/">
@@ -35,17 +46,7 @@ const Contact = () => {
                   </a>
                   </span>
             </div>
-            <div className="callout callout--primary">
-                <div className="grid grid--1x2" data-aos="fade-up-left">
-                <div className="callout__content">
-                 <h2 className="callout__heading">Ready to Get in touch?</h2>
-                 <p>
-                   I am eager to work with you and create great technology. 
-                  </p>
-                 </div>
-                  <a href="mailto: jonathan.plaisted@outlook.com" className="btn btn--stretched">Contact</a>
-              </div>
-             </div>
+        
             </section>
          );
     }
