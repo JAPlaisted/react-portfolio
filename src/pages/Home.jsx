5454619b24a6a916243b2d77a5dfd38cfcf8348d
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-11-15 10:44:27 -0800

    Commented out AI Art piece

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ class Home extends Component {
                 <Header />
                 <Projects />
                 <Skills />
-                <AIArt />
+                {/*<AIArt />*/}
                 <Bio/>
                 <Contact />
                 <Footer />
